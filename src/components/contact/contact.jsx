211c0b39a055e7b08e547170b09e3c36b5c84c3b
sarthak <sarthak613singh@gmail.com>
2024-05-18 01:47:32 +0530

    chore: Update npm dependencies for emailjs to latest versions

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,4 +1,6 @@
 import "./contact.scss";
+import emailjs from "@emailjs/browser";
+import { useRef, useState } from "react";
 import { motion } from "framer-motion";
 
 function Contact() {
@@ -15,6 +17,40 @@ function Contact() {
         staggerChildren: 0.2,
       },
     },
+  };
+
+  const form = useRef();
+  const name = useRef();
+  const email = useRef();
+  const message = useRef();
+
+  const [success, setSuccess] = useState(false);
+  const [error, setError] = useState({
+    e: false,
+    message: "",
+  });
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm("service_sz0y7cj", "template_a8o27ov", form.current, {
+        publicKey: "_UAqXaW9Gn-Nsu3lO",
+      })
+      .then(
+        () => {
+          setSuccess(true);
+          message.current.value = "";
+          email.current.value = "";
+          name.current.value = "";
+        },
+        (error) => {
+          setError({
+            e: true,
+            message: error.text,
+          });
+        }
+      );
   };
 
   return (
@@ -52,6 +88,7 @@ function Contact() {
                 initial={{ pathLength: 0 }}
                 whileInView={{ pathLength: 1 }}
                 transition={{ duration: 6 }}
+                stroke={0.2}
                 d="M4,24v-5H0V0h23v19h-9.3L4,24z M2,17h4v3.7l7.3-3.7H21V2H2V17z"
               />
             </g>
@@ -67,14 +104,64 @@ function Contact() {
           </svg>
         </motion.div>
 
+        {success && (
+          <motion.div className="success">
+            <svg
+              stroke="0.1"
+              width="400px"
+              height="400px"
+              fill="none"
+              viewBox="0 0 36 36"
+            >
+              <motion.path
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 3, delay: 4 }}
+                d="M18,2A16,16,0,1,0,34,18,16,16,0,0,0,18,2Zm0,30A14,14,0,1,1,32,18,14,14,0,0,1,18,32Z"
+              ></motion.path>
+              <motion.path
+                initial={{ pathLength: 0 }}
+                whileInView={{ pathLength: 1 }}
+                transition={{ duration: 3, delay: 4 }}
+                d="M28,12.1a1,1,0,0,0-1.41,0L15.49,23.15l-6-6A1,1,0,0,0,8,18.53L15.49,26,28,13.52A1,1,0,0,0,28,12.1Z"
+              ></motion.path>
+            </svg>
+          </motion.div>
+        )}
+
         <motion.form
           initial={{ opacity: 0 }}
-          whileInView={{ opacity: 1 }}
-          transition={{ delay: 4, duration: 1.5 }}
+          whileInView={{ opacity: success ? 0 : 1 }}
+          transition={{ delay: 4 }}
+          ref={form}
+          onSubmit={sendEmail}
         >
-          <input type="text" placeholder="Name" name="name" required />
-          <input type="email" placeholder="Email" name="email" required />
-          <textarea rows={8} name="message" placeholder="Message"></textarea>
+          <input
+            ref={name}
+            type="text"
+            placeholder="Name"
+            name="name"
+            required
+          />
+          <input
+            ref={email}
+            type="email"
+            placeholder="Email"
+            name="email"
+            required
+          />
+          <textarea
+            ref={message}
+            rows={8}
+            name="message"
+            placeholder="Message"
+          ></textarea>
+          {error.e && (
+            <div className="error">
+              <span>Error:</span> {error.message}
+            </div>
+          )}
+
           <input type="submit" value="Submit" />
         </motion.form>
       </div>
